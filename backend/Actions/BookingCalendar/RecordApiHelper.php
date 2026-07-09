51e0b70c0f2535835f7d0ab236751e211a64d541
Rishad Alam <sazzadullalamrishad@gmail.com>
2026-07-09 16:41:22 +0600

    fix(RecordApiHelper): correct typo in plugin activation message

--- backend/Actions/BookingCalendar/RecordApiHelper.php
+++ backend/Actions/BookingCalendar/RecordApiHelper.php
@@ -32,7 +32,7 @@ class RecordApiHelper
         $mainAction = $this->_integrationDetails->mainAction ?? 'create_booking';
         $defaultResponse = [
             'success' => false,
-            'message' => wp_sprintf(__('%s plugin is not installed or activate', 'bit-integrations'), 'Bit Integrations Pro')
+            'message' => wp_sprintf(__('%s plugin is not installed or activated', 'bit-integrations'), 'Bit Integrations Pro')
         ];
 
         switch ($mainAction) {
